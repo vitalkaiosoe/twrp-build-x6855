d5cc57dbc2a08ef3ac94b5f0f3d15bad8e05c2d5
vitalkaiosoe <vitalkaclassroom@gmail.com>
2025-10-25 02:02:51 +0300

    Delete device/infinix directory

--- device/infinix/device.mk
+++
@@ -1,8 +0,0 @@
-#
-# Copyright (C) 2022 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-# Inherit from mt6789-common
-$(call inherit-product, device/transsion/mt6789-common/common.mk)
